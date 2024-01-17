1b314e576b7652e8b5884aca04112b408f0aa9f0
Nihal-Abedin <nihal.abedin.annon99@gmail.com>
2024-01-18 03:02:37 +0600

    forms refactor and inputs

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -4,6 +4,8 @@ import { useNavigate, Link } from 'react-router-dom';
 import { useAuthStore } from '../../store/auth';
 import { register } from '../../utils/auth';
 import { useRegisTration } from '../../queryHooks/auth/useAuth';
+import Input from '../views/Input';
+import Button from '../views/Button';
 
 const Register = () => {
     const navigate = useNavigate();
@@ -31,118 +33,64 @@ const Register = () => {
         );
     };
     return (
-        <main className='' style={{ marginBottom: 100, marginTop: 50 }}>
-            <div className='container'>
-                {/* Section: Login form */}
-                <section className=''>
-                    <div className='row d-flex justify-content-center'>
-                        <div className='col-xl-5 col-md-8'>
-                            <div className='card rounded-5'>
-                                <div className='card-body p-4'>
-                                    <h3 className='text-center'>Register Account</h3>
-                                    <br />
+        <div className='h-screen w-screen flex justify-center items-center shadow-lg '>
+            <div className='shadow-black-300/100 m-1 w-96 bg-white px-10 py-6 shadow-lg rounded-lg'>
+                <h3 className='my-5 text-center font-medium leading-5'>Register</h3>
 
-                                    <div className='tab-content'>
-                                        <div
-                                            className='tab-pane fade show active'
-                                            id='pills-login'
-                                            role='tabpanel'
-                                            aria-labelledby='tab-login'>
-                                            <form onSubmit={handleRegister}>
-                                                <div className='form-outline mb-4'>
-                                                    <label className='form-label' htmlFor='Full Name'>
-                                                        Full Name
-                                                    </label>
-                                                    <input
-                                                        type='text'
-                                                        id='username'
-                                                        placeholder='Full Name'
-                                                        required
-                                                        className='form-control'
-                                                        onChange={(e) => setFull_name(e.currentTarget.value)}
-                                                    />
-                                                </div>
-                                                <div className='form-outline mb-4'>
-                                                    <label className='form-label' htmlFor='loginName'>
-                                                        Email
-                                                    </label>
-                                                    <input
-                                                        type='email'
-                                                        id='email'
-                                                        placeholder='Email Address'
-                                                        required
-                                                        className='form-control'
-                                                        onChange={(e) => setEmail(e.currentTarget.value)}
-                                                    />
-                                                </div>
-
-                                                <div className='form-outline mb-4'>
-                                                    <label className='form-label' htmlFor='loginName'>
-                                                        Mobile Number
-                                                    </label>
-                                                    <input
-                                                        type='text'
-                                                        id='phone'
-                                                        placeholder='Mobile Number'
-                                                        required
-                                                        className='form-control'
-                                                        onChange={(e) => setPhone(e.currentTarget.value)}
-                                                    />
-                                                </div>
-                                                <div className='form-outline mb-4'>
-                                                    <label className='form-label' htmlFor='loginPassword'>
-                                                        Password
-                                                    </label>
-                                                    <input
-                                                        type='password'
-                                                        id='password'
-                                                        placeholder='Password'
-                                                        className='form-control'
-                                                        onChange={(e) => setPassword(e.currentTarget.value)}
-                                                    />
-                                                </div>
-                                                {/* Password input */}
-                                                <div className='form-outline mb-4'>
-                                                    <label className='form-label' htmlFor='loginPassword'>
-                                                        Confirm Password
-                                                    </label>
-                                                    <input
-                                                        type='password'
-                                                        id='confirm_password'
-                                                        placeholder='Confirm Password'
-                                                        required
-                                                        className='form-control'
-                                                        onChange={(e) => setConfirm_password(e.currentTarget.value)}
-                                                    />
-                                                </div>
-                                                {/* Password Check */}
-                                                {/* <p className='fw-bold text-danger'>
+                <form onSubmit={handleRegister} className='justify-center align-center mt-7 flex flex-col gap-3'>
+                    <Input
+                        type='text'
+                        id='username'
+                        placeholder='Full Name'
+                        required
+                        className='form-control'
+                        onChange={(e) => setFull_name(e.currentTarget.value)}
+                    />
+                    <Input
+                        type='email'
+                        id='email'
+                        placeholder='Email Address'
+                        required
+                        className='form-control'
+                        onChange={(e) => setEmail(e.currentTarget.value)}
+                    />
+                    <Input
+                        type='text'
+                        id='phone'
+                        placeholder='Mobile Number'
+                        required
+                        className='form-control'
+                        onChange={(e) => setPhone(e.currentTarget.value)}
+                    />
+                    <Input
+                        type='password'
+                        id='password'
+                        placeholder='Password'
+                        className='form-control'
+                        onChange={(e) => setPassword(e.currentTarget.value)}
+                    />
+                    <Input
+                        type='password'
+                        id='confirm_password'
+                        placeholder='Confirm Password'
+                        required
+                        className='form-control'
+                        onChange={(e) => setConfirm_password(e.currentTarget.value)}
+                    />
+                    {/* Password Check */}
+                    {/* <p className='fw-bold text-danger'>
                                                     {password2 !== password ? 'Passwords do not match' : ''}
                                                 </p> */}
-
-                                                <button
-                                                    className='btn btn-primary w-100'
-                                                    type='submit'
-                                                    disabled={isPending}>
-                                                    <span className='mr-2'>Sign Up</span>
-                                                    <i className='fas fa-user-plus' />
-                                                </button>
-
-                                                <div className='text-center'>
-                                                    <p className='mt-4'>
-                                                        Already have an account? <Link to='/login/'>Login</Link>
-                                                    </p>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                    <Button element='button' type='submit' disabled={isPending}>
+                        <span className='mr-2'>Sign Up</span>
+                        <i className='fas fa-user-plus' />
+                    </Button>
+                    <Link to='/login/' className='mt-5 cursor-pointer text-center text-blue-500 '>
+                        Already have an account? Login
+                    </Link>
+                </form>
             </div>
-        </main>
+        </div>
     );
 };
 
--- src/components/views/Input.jsx
+++ src/components/views/Input.jsx
@@ -1,13 +1,17 @@
-import { twMerge } from 'tailwind-merge'
+import { twMerge } from 'tailwind-merge';
 
-const Input = ({ type = 'text', onChange=()=>{}, defaultValue, placeholder, className }) => {
+const Input = ({ type = 'text', onChange = () => {}, defaultValue, placeholder, className, ...restProps }) => {
     return (
         <input
-            className={twMerge(' w-full bg-[#F7F8FA] rounded-sm py-2 px-4 outline-none border border-[#001F2D]',className)}
+            className={twMerge(
+                ' w-full bg-[#F7F8FA] rounded-sm py-2 px-4 outline-none border border-[#001F2D]',
+                className
+            )}
             type={type}
             defaultValue={defaultValue}
             placeholder={placeholder}
             onChange={(e) => onChange(e)}
+            {...restProps}
         />
     );
 };
